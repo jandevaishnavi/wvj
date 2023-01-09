c5ec261c4be7cf750c0a12a760c2fd89ea3e122d
jandevaishnavi <113428453+jandevaishnavi@users.noreply.github.com>
2023-01-09 12:50:49 +0530

    Add files via upload

---
+++ donor/viewrequest.php
@@ -0,0 +1,138 @@
+<?php if(!isset($_SESSION)) {session_start();}  ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="css/lightbox.css" rel="stylesheet" />
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--slider-->
+<link href="css/flexslider.css" rel="stylesheet" type="text/css" media="all" />
+     <script src="js/jquery-1.11.0.min.js"></script>
+	<script src="js/lightbox.min.js"></script>
+<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+<script src="js/jquery.flexslider.js" type="text/javascript"></script>
+  
+ <script type="text/javascript">
+     $(function () {
+         SyntaxHighlighter.all();
+     });
+     $(window).load(function () {
+         $('.flexslider').flexslider({
+             animation: "slide",
+             animationLoop: false,
+             itemWidth: 210,
+             itemMargin: 5,
+             minItems: 2,
+             maxItems: 4,
+             start: function (slider) {
+                 $('body').removeClass('loading');
+             }
+         });
+     });
+  </script>
+  <style type="text/css">
+  #grad1{
+	height:100px;
+	background-color:red;
+	background:linear-gradient(hotpink,coral);
+}
+</style>
+</head>
+
+<body>
+
+<?php
+
+if($_SESSION['donorstatus']=="")
+{
+	header("location:../login.php");
+}
+?>
+<?php include('function.php'); ?>
+
+ <div class="h_bg">
+<div class="wrap">
+<div class="header">
+		<div class="logo">
+<div id="grad1"> </div>
+
+		</div>
+	</div>
+</div>
+</div>
+<div class="nav_bg">
+<div class="wrap">
+	<ul class="nav">
+	<div id="grad1"> 
+			<li class="active"><a href="chngpwd.php">Change Password</a></li>	
+			<li><a href="updateprofile.php">Update Profile</a></li>            
+			<li><a href="blooddonated.php">Blood Donated</a></li>
+            <li><a href="viewdonations.php">View Donations</a></li>
+            <li><a href="viewrequest.php">View Requestes</a></li>
+            <li><a href="logout.php">log Out</a></li>
+           </div>
+            </ul>
+	</div>
+<div style="height:1400px; width:1400px; margin:auto; margin-top:10px; margin-bottom:50px; background-color:#ffccff; border:2px solid red; box-shadow:4px 1px 20px black;">
+     <form method="post" enctype="multipart/form-data">
+ <table cellpadding="25" cellspacing="10" width="300px" height="1000px"  style="margin:auto" >
+
+   <tr><td colspan="7" align="center"><img src="Images/brequest.png" height="100px" /></td></tr>
+
+   <tr><td>&nbsp;</td><td>&nbsp;</td></tr>   
+ <tr style="background-color:bisque" align="center" class="bold">            
+             <td class="bold" style="color:red"  >Blood Group</td><td align="center">Name</td><td align="center">Gender</td><td align="center">age</td><td align="center">Mobile No</td><td align="center">Email</td>
+            <td align="center">bg id </td>
+        </tr>
+                   
+
+
+
+<?php
+
+	
+$cn=mysqli_connect("localhost","root","","bloodbank");
+$s="select * from requestes";
+	$result=mysqli_query($cn,$s);
+	$r=mysqli_num_rows($result);
+	//echo $r;
+	while($data=mysqli_fetch_array($result))
+	{
+				echo"<tr><td  style=' padding-left:50px'>$data[0]</td><td  style=' padding-left:10px'>$data[1]</td><td  style=' padding-left:20px'>$data[2]</td><td  style=' padding-left:30px'>$data[3]</td><td  style=' padding-left:50px'>$data[4]</td><td  style=' padding-left:50px'>$data[5]</td><td  style=' padding-left:60px'>$data[6]</td></tr>";
+			}
+			mysqli_close($cn);
+			?>
+
+
+</table>
+</form>
+        </div>
+          
+        <div class="clear"></div>
+<div class="ftr-bg">
+<div class="wrap">
+<div class="footer">
+	<div class="f_nav">
+		
+	</div>
+		<div class="copy">
+			<p class="title">© All Rights Reserved | Design by Vaishnavi Jande |</p>
+		</div>
+	<div class="clear"></div>
+</div>
+</div>
+</div>
+
+
+
+
+			
+			
+	
+</body>
+</html>
